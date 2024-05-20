12b2a9bb88bfd323ea5a68e62070d558bd632c70
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-05-20 11:30:27 +0530

    Day 2: Learned about objects

--- javascript/object.js
+++ javascript/object.js
@@ -4,5 +4,35 @@ let myobj={
     pageCount: 500
 }
 // console.log(typeof(myobj.title));
-console.log(`${myobj.title} launched in ${myobj.year} with ${myobj.pageCount} pages`);
+// console.log(`${myobj.title} launched in ${myobj.year} with ${myobj.pageCount} pages`);
 
+let myob2={
+    title: 'ansi c',
+    year : 2006,
+    pageCount: 300
+}
+
+let func=function(book){
+    return{
+        summary: `${book.title} is released in the year ${book.year}`,
+        pageCountSummary: `${book.title} is released in the year ${book.year} which is ${book.pageCount} pages long`
+    }
+}
+
+let book1=func(myobj)
+let book2=func(myob2)
+
+console.log(book1.summary);
+console.log(book1.pageCountSummary);
+
+
+//Challenge area
+let conv=function(farenhite){
+    return{
+        farenhite: farenhite,
+        celsius:( (farenhite-32) * 5 /9),
+        kelvin: (farenhite + 459.6) * 5 / 9
+    }
+}
+
+console.log(conv(200).farenhite);
