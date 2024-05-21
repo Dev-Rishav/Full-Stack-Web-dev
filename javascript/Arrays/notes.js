24b510d4f80feed057512c22a4581c9e372a2b6a
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-05-21 17:01:17 +0530

    day3: Initialize arrays

---
+++ javascript/Arrays/notes.js
@@ -0,0 +1,4 @@
+let notes=['Notes 1','Notes 2','Notes 3']
+
+console.log(notes);
+console.log(notes.length);
---
+++ javascript/Arrays/todo.js
@@ -0,0 +1,3 @@
+let todo=['walk the dog','GYM','compete js asap']
+
+console.log(`Your first task is ${todo[0]} and second last task is ${todo[todo.length-2]}`);
