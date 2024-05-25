9d0eb5fa4dd50e47f91c01a61d9d8d67e7a01b5a
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-05-25 12:06:58 +0530

    Day 4: Array Objects

--- javascript/Arrays/todo1.js
+++ javascript/Arrays/todo1.js
@@ -1,6 +1,6 @@
 const todos = ['order cat food','clean kitchen','buy food','do work','exercise']
 
-const todoObj=[]
+let todoObj=[]
 for(let i=0;i<todos.length;i++){
     let obj={
         title: todos[i],
@@ -10,6 +10,13 @@ for(let i=0;i<todos.length;i++){
 }
 
 // console.log(todoObj);
-let removeObj=function(objArray){
-        
+let removeObj=function(objAr,objTitle){
+    todoObj.forEach(function(item,index){
+        if(item.title.toLowerCase() === objTitle.toLowerCase())
+            todoObj.splice(index,1);
+    })
 }
+console.log(todoObj);
+removeObj(todoObj,'clean kitchen')
+// console.log(`After deleting the object, the new array is ${todoObj}`);
+console.log(todoObj);
