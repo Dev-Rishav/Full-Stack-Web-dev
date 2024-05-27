eed8d4cae48db38a71c324f081c96e04e75aadf7
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-05-27 13:10:49 +0530

    Day 7: Render dynamic notes and implement search function to TODO

--- javascript/notes-app/todo app/todo.js
+++ javascript/notes-app/todo app/todo.js
@@ -22,6 +22,25 @@ const todos=[{
     completed: true
 }]
 
+const filter={
+    searchText:''
+}
+
+const renderNotes=function(todos,filter){
+    const filteredNotes = todos.filter(function(notes){
+        return notes.text.toLowerCase().includes(filter.searchText.toLowerCase());
+    })     
+    // console.log(filteredNotes);
+    document.querySelector('#notes').innerHTML=''; //this clears the screen to initial condition
+    filteredNotes.forEach(function(note){
+        const pl=document.createElement('p');
+        pl.textContent=note.text;
+        document.querySelector('#notes').appendChild(pl)
+    })
+}
+
+//initial rendering
+renderNotes(todos,filter);
 
 let count=0;
 todos.forEach(function (ele){
@@ -33,13 +52,19 @@ const ps=document.createElement('h2');
 ps.textContent=`You have ${count} todos left`
 document.querySelector('body').appendChild(ps)
 
-todos.forEach(function(todo){
-        const x=document.createElement('p');
-        x.textContent=todo.text;
-        document.querySelector('body').appendChild(x);
-})
+// todos.forEach(function(todo){
+//         const x=document.createElement('p');
+//         x.textContent=todo.text;
+//         document.querySelector('body').appendChild(x);
+// }) //initially rendered already
 
 document.querySelector('#addTODO').addEventListener('click',function(e){
     console.log("Add a todo");
 })
 
+
+//Listen for todo text change
+document.querySelector('#addnewTodo').addEventListener('input',function(e){
+    filter.searchText=e.target.value;
+    renderNotes(todos,filter)   //rerender the notes
+})
