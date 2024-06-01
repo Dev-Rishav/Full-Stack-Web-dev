c4d7a1147b1043c346b8e5a42c21884b561373a1
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-01 13:46:28 +0530

    Day 11: Introduction to components

--- React/01basics/src/App.jsx
+++ React/01basics/src/App.jsx
@@ -1,7 +1,29 @@
+import Chai from "./Chai"
+
 
 function App() {
   return (
-    <h1>Chai aur react with vite | Rishav</h1>
+    // <h1>Chai aur react with vite | Rishav</h1>
+    
+
+    //in jsx only one element can be returned
+    // <Chai/>  //tag name always starts with uppercase
+
+    //so to overcome this situation we can use <div> tag to enclose 
+    //multiple elements
+    // <div>
+    //   <Chai/>
+    //   <p>Paragraph is working</p>
+    //   <h1>hello</h1>
+    // </div>
+
+    //instead of using <div> tag we will shift to <> tag which is
+    //referred as fragments in jsx
+
+    <>
+    <Chai/>
+    <p>Para</p>
+    </>
   )
 }
 
---
+++ React/01basics/src/Chai.jsx
@@ -0,0 +1,12 @@
+function Chai(){        //function name in jsx starts with uppercase
+    return(
+        <h3>Chai is ready</h3>
+    )
+}
+
+export default Chai
+
+
+/*basically jsx is used to define components in react 
+and vite only allow us to write new js scripts as 
+components so we need to name script file as jsx*/
