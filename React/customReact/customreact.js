14738590f92613c259b0a0a4b5059957d48d50b3
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-06 15:06:18 +0530

    Clean up and create custom react lib

--- React/customReact/customreact.js
+++ React/customReact/customreact.js
@@ -1,5 +1,22 @@
 
+function customRender (reactElement,container){/*
+    const domElement= document.createElement(reactElement.type)
+    domElement.innerHTML =reactElement.children;
+    domElement.setAttribute('href',reactElement.props.href);
+    domElement.setAttribute('target',reactElement.props.target)
 
+    container.appendChild(domElement);*/ //handles only href and target attributes
+
+    const domElement=document.createElement(reactElement.type);//create a new element of reactElement type
+    domElement.innerHTML=reactElement.children  //copy the children
+    for(const prop in reactElement.props){
+        if(prop === 'children')
+            continue;
+        domElement.setAttribute(prop,reactElement.props[prop]);
+        }
+    container.appendChild(domElement)
+    //dynamically sets all the attributes except `children`
+}
 const reactElement = {
     type : 'a',
     props: 
