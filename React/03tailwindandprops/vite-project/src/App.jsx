c8e644dec30586a71915eb0d6dee8f48782ba683
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-14 04:30:10 +0530

    passing multiple parameters with default values and using them in components

--- React/03tailwindandprops/vite-project/src/App.jsx
+++ React/03tailwindandprops/vite-project/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card name="rishav" someObj={myArr}/>
-      <Card/>
+      <Card userName="rishav" btnText="click me"/>
+      <Card userName="YAYA"  btnText="visit me"/>
 
     </>
   )
