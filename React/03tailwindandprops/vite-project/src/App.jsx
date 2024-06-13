120d28a52b02583887e9f28d9670d61562ef69a0
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-14 04:21:36 +0530

    pass objects into props

--- React/03tailwindandprops/vite-project/src/App.jsx
+++ React/03tailwindandprops/vite-project/src/App.jsx
@@ -6,10 +6,15 @@ import Card from './components/Card'
 
 function App() {
 
+  const myObj={
+    name: "Rishav",
+    Age: 23
+  }
+
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card name="rishav"/>
+      <Card name="rishav" someObj={myObj}/>
       <Card/>
 
     </>
