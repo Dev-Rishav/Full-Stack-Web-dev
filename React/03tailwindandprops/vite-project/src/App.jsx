f06e53c9eead0377014dcca0bd27b7f9b720ff8d
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-14 04:19:23 +0530

    Need of props: When we need to make multiple cards and have to render data on them separately, we use props to pass the values from app.jsx to component.jsx as an object

--- React/03tailwindandprops/vite-project/src/App.jsx
+++ React/03tailwindandprops/vite-project/src/App.jsx
@@ -2,34 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card name="rishav"/>
+      <Card/>
 
-
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/25477056/pexels-photo-25477056/free-photo-of-snow-leopard-lying-down.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512"/>
-  <div class="pt-6 space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 da">
-        Sarah Dayan
-      </div>
-      <div>
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
     </>
   )
 }
