eccfb5744db4907e6dbf470a778d18c7741eff09
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-19 00:15:50 +0530

    link custom hook useCurrencyInfo to app.jsx and fetch the object from api

--- React/currency-converter/src/App.jsx
+++ React/currency-converter/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-import InputBox from './Components/InputBox'
+import React, { useState } from 'react'
+import { InputBox } from './Components'
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
 
 const App = () => {
+
+  const [amount,setAmount] = useState(0);
+  const [from,setFrom]= useState("usd")
+  const [to, setTo]= useState("inr");
+  const [convertedAmount, setConvertedAmount]= useState(0);
+
+
+  const currencyInfo=useCurrencyInfo(from); 
+
+  const options= Objects.keys(currencyInfo);//this will return all the keys of the objects 
+  
+
+
   return (
     <div className=''>
       <InputBox/>
