15befd082f49ee8ead97962b6950effec034603b
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-26 00:03:07 +0530

    Connect navlink to the routes using "to"

--- React/07ReactRouter/src/components/header/Header.jsx
+++ React/07ReactRouter/src/components/header/Header.jsx
@@ -35,6 +35,7 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
+                                to="/" /*Telling navlink where to go */
                                     className={(isActive) =>
                                         `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }   //here we are using call back function in css, cause if we want to change the color of the text when hovered then this call back function provides a property call "isActive"
@@ -42,6 +43,17 @@ export default function Header() {
                                 >
                                     Home
                                 </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to="/about" /*Telling navlink to visit /about */
+                                //this to will automatically inject data to isActive variable 
+                                
+                                    className={(isActive) =>
+                                        `${isActive ? "text-orange-700"  : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }>
+                                        About
+                                    </NavLink>
                             </li>
 
 
