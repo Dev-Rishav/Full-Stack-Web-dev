91847789a3879ad7fe60f05ef1de9abb51dfd07a
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-08-08 23:49:58 +0530

    Node: SYnc sources

--- Node/weatherApp/app.js
+++ Node/weatherApp/app.js
@@ -6,3 +6,14 @@ request({url:url,json:true},(error,response)=>{ //json property automatically co
     console.log(response.body.current);
     // console.log("It is currently",response.body.current.temp_c," degrees out. It feels like",response.body.current.feelslike_c," degrees out.");
 })
+
+
+const geolocation = (data,callback)=>{
+    const query=data;
+    const geoUrl="https://us1.locationiq.com/v1/search?key=pk.1e2b8534b90d68587fb6032dd1d927c6&q="+query+"&format=json&"
+    callback(geoUrl);
+}
+
+const weatherFetch=()=>{
+        geolocation()
+}
