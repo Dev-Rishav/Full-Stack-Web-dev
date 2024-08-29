d99aae80d121b03c7452ce2b9ea1af53edeaa9bc
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-08-29 22:42:15 +0530

    middlewares

--- Node/Server/06Middlewares.js
+++ Node/Server/06Middlewares.js
@@ -2,8 +2,29 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+//Middlewares
+//*Middlewares are middle man that interact between client and server side. 
+//? middlewares runs on every req,res.
+//? Middlewares have the functionality to handle req,res and can return from it.
+//? middleware execute one after another and then the rest of the script kicks in usinfg the next().
+//? Middleware use next() to call the remaining script
+//! if neither return nor next() is defined into the last middlware then the api will crash and it will wait for http methods for infinite amount of time.
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//custom middleware-1
+app.use((req,res,next)=>{
+    console.log("Hello from middleware 1");
+    //return res.json({status:"completed"});  //! this will end the script here and res will be retuned to the client side.
+    req.myUsername="Rishav"; //? this binds a new property called myUsername with req object and this can be accessed anywhere in the script.
+    next() //* next middleware is executed
+})
+
+app.use((req,res,next)=>{
+    console.log("hello from middleware 2",req.myUsername);
+    next(); //* This ensures that rest of the script runs
+})
 
 const users = require('./MOCK_DATA.json');
 const { log } = require('console');
