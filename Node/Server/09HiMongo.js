50febbdbf2f0a1b65190bef0584f20f7e67c7748
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-08-31 01:36:07 +0530

    hi Mongo

---
+++ Node/Server/09HiMongo.js
@@ -0,0 +1,10 @@
+//* NO-SQL Document Based Database
+//* Strong Support for Aggregation Pipes
+//* Works oin BSON format
+//* Best for Node Application
+
+//? show dbs -> Shows all the databases that are present.
+//? use <db_name> -> to switch to a another datbase. by default you will be on "test" database.
+//? show collections -> shows all the collection in the current database.
+//? db.coll.find({}) -> shows all the values in that collection
+//? db.coll.insert({}) -> insert the following object to that collection.
